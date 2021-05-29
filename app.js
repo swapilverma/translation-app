0a35d67f89981e272d1e18f513d297968a57d951
Swapil Verma <swapilverma23@gmail.com>
2021-05-29 21:12:31 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var output = document.querySelector("output");
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
+

7ea6f0cfd9e8e118b53a699896cb53b659ee6da1
Swapil Verma <swapilverma23@gmail.com>
2021-05-30 16:13:20 +0530

    added files
    style.css and app.js files are added

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
-var output = document.querySelector("output");
+var outputDiv = document.querySelector("output");
 
 var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
+function getTranslation(input){
+
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something is wrong with server! Try again after some time")
+}
+
+
+function clickHandler(){
+    var inputText = textInput.value;
+    console.log("clicked")
+
+    fetch(getTranslation(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+        .catch(errorHandler)
+}
+
+
+
+btnTranslate.addEventListener("click", clickHandler )
